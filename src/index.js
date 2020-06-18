bd0ea6d5728a91466b03f80a2a6b8d88032fd2b8
Oliver Lorenz <mail@oliverlorenz.com>
2020-06-18 13:36:24 +0200

    feat: initial version

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
-// if (process.cwd() === process.env.INIT_CWD) process.exit(0);
-
 const base = require('@high-standards-nodejs/base');
-base.default.checkAcceptedHighStandards();
+const path = require('path');
+
+base.checkAcceptedHighStandards();
+
+const packageJsonOfConfig = base.getInitiatingProjectPackageJson();
+
+if (!packageJsonOfConfig.engines) packageJsonOfConfig.engines = {};
+if (!packageJsonOfConfig.engines.node) packageJsonOfConfig.engines.node = `>=${process.versions.node}`;
+
+const templateContent = JSON.parse(
+    base.getTemplate(
+        __dirname, 
+        'package.json', 
+        {
+            newestTypescriptVersion: "1.0.0",
+            nodeEngineVersion: `${packageJsonOfConfig.engines.node.replace(/^\W+/, '')}`
+        }
+    )
+);
+
+if (!packageJsonOfConfig.dependencies) packageJsonOfConfig.dependencies = {};
+
+[
+    'typescript',
+    '@types/node',
+]
+.forEach((packageName) => {
+    packageJsonOfConfig.dependencies[packageName] = templateContent.dependencies[packageName];
+})
+console.log(process.versions.node)
+base.writeFile(
+    path.join(
+        base.getProjectRoot(), 
+        'package.json'
+    ),
+    JSON.stringify(packageJsonOfConfig, null, 2)
+);
