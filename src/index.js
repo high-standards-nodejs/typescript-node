95e5ccefb0f39806f8823cd3fd143e10e27f5aef
Oliver Lorenz <mail@oliverlorenz.com>
2020-06-18 14:09:25 +0200

    fix: version bump @high-standards-nodejs/base

--- src/index.js
+++ src/index.js
@@ -1,38 +1,40 @@
 const base = require('@high-standards-nodejs/base');
 const path = require('path');
 
-base.checkAcceptedHighStandards();
+(async() => {
+    await base.checkAcceptedHighStandards();
 
-const packageJsonOfConfig = base.getInitiatingProjectPackageJson();
-
-if (!packageJsonOfConfig.engines) packageJsonOfConfig.engines = {};
-if (!packageJsonOfConfig.engines.node) packageJsonOfConfig.engines.node = `>=${process.versions.node}`;
-
-const templateContent = JSON.parse(
-    base.getTemplate(
-        __dirname, 
-        'package.json', 
-        {
-            newestTypescriptVersion: "1.0.0",
-            nodeEngineVersion: `${packageJsonOfConfig.engines.node.replace(/^\W+/, '')}`
-        }
-    )
-);
-
-if (!packageJsonOfConfig.dependencies) packageJsonOfConfig.dependencies = {};
-
-[
-    'typescript',
-    '@types/node',
-]
-.forEach((packageName) => {
-    packageJsonOfConfig.dependencies[packageName] = templateContent.dependencies[packageName];
-})
-console.log(process.versions.node)
-base.writeFile(
-    path.join(
-        base.getProjectRoot(), 
-        'package.json'
-    ),
-    JSON.stringify(packageJsonOfConfig, null, 2)
-);
+    const packageJsonOfConfig = base.getInitiatingProjectPackageJson();
+    
+    if (!packageJsonOfConfig.engines) packageJsonOfConfig.engines = {};
+    if (!packageJsonOfConfig.engines.node) packageJsonOfConfig.engines.node = `>=${process.versions.node}`;
+    
+    const templateContent = JSON.parse(
+        base.getTemplate(
+            __dirname, 
+            'package.json', 
+            {
+                newestTypescriptVersion: "1.0.0",
+                nodeEngineVersion: `${packageJsonOfConfig.engines.node.replace(/^\W+/, '')}`
+            }
+        )
+    );
+    
+    if (!packageJsonOfConfig.dependencies) packageJsonOfConfig.dependencies = {};
+    
+    [
+        'typescript',
+        '@types/node',
+    ]
+    .forEach((packageName) => {
+        packageJsonOfConfig.dependencies[packageName] = templateContent.dependencies[packageName];
+    })
+    console.log(process.versions.node)
+    base.writeFile(
+        path.join(
+            base.getProjectRoot(), 
+            'package.json'
+        ),
+        JSON.stringify(packageJsonOfConfig, null, 2)
+    );
+})()
